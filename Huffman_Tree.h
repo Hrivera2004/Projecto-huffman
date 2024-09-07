3ca9c02c72c8affc49475e98ca3a8d7c1997354d
hector Rivera <hrolriverav@gmail.com>
2024-09-06 18:15:53 -0600

    Empezando algorithmo del arbol huffman'

--- Huffman_Tree.h
+++ Huffman_Tree.h
@@ -16,7 +16,7 @@ public:
 	Huffman_Tree() {
 
 	}
-	int lectura_txt(string filename) {
+	int lectura_txt(string filename) {//probado
 		ifstream lectura;
 		lectura.open(filename);
 		if (!lectura) {
@@ -34,12 +34,27 @@ public:
 		print();
 		return 0;
 	}
-	void print() {
+	
+	void build_tree() {//falta probar
+		while (cola.size() > 1){
+			cola.push_back(inner_node_create());
+			//llamar ordenamiento
+		}
+	}
+
+	Nodo* inner_node_create() {//falta probar
+		Nodo* nodo1 = cola[cola.size() - 1];
+		Nodo* nodo2 = cola[cola.size() - 2];
+		cola.pop_back();
+		cola.pop_back();
+		return new Nodo('\0',nodo1->getFreq() + nodo2->getFreq(), nodo2, nodo1);
+	}
+	void print() {//probado
 		for (size_t i = 0; i < cola.size(); i++){
 			cout << cola[i]->getChar()<< " " << cola[i]->getFreq()<< endl;
 		}
 	}
-	bool find(char chr) {
+	bool find(char chr) {//probado
 		for (size_t i = 0; i < cola.size(); i++){
 			if (cola[i]->getChar() == chr) {
 				cola[i]->increase_Freq();
