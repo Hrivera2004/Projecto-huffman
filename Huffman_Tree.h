8a23feb1d42a795c8ff40288a69a29e9dee952b5
hector Rivera <hrolriverav@gmail.com>
2024-09-08 11:07:32 -0600

    cambios

--- Huffman_Tree.h
+++ Huffman_Tree.h
@@ -5,17 +5,17 @@
 #include <iomanip>
 #include <fstream>
 #include "Nodo.h"
-
 using namespace	std;
 
 class Huffman_Tree {
 private:
-	Nodo* head;
+	Nodo* head = NULL;
 	vector<Nodo*> cola; // implementar como cola sino toca usar un quicksort (talvez no)
 public:
 	Huffman_Tree() {
 
 	}
+
 	int lectura_txt(string filename) {//probado
 		ifstream lectura;
 		lectura.open(filename);
@@ -25,20 +25,20 @@ public:
 		}
 		char caracter = ' ';
 		while (lectura.get(caracter)) {
-			if (!find(caracter)){
+			if (!find(caracter)) {
 				Nodo* nodo = new Nodo(caracter);
 				cola.push_back(nodo);
 			}
 		}
-		head = cola[0]; 
-		print();
+		head = cola[0];
+		lectura.close();  // Cerrar archivo después de leer
 		return 0;
 	}
-	
+
 	void build_tree() {//falta probar
-		while (cola.size() > 1){
+		while (cola.size() > 1) {
 			cola.push_back(inner_node_create());
-			//llamar ordenamiento
+			ordenarCola();
 		}
 	}
 
@@ -47,15 +47,18 @@ public:
 		Nodo* nodo2 = cola[cola.size() - 2];
 		cola.pop_back();
 		cola.pop_back();
-		return new Nodo('\0',nodo1->getFreq() + nodo2->getFreq(), nodo2, nodo1);
+		return new Nodo('\0', nodo1->getFreq() + nodo2->getFreq(), nodo2, nodo1);
 	}
-	void print() {//probado
-		for (size_t i = 0; i < cola.size(); i++){
-			cout << cola[i]->getChar()<< " " << cola[i]->getFreq()<< endl;
+
+	void print() {
+		cout << "Tabla de frecuencias de caracteres:\n";
+		for (size_t i = 0; i < cola.size(); i++) {
+			cout << "'" << cola[i]->getChar() << "' aparece " << cola[i]->getFreq() << " veces." << endl;
 		}
 	}
+
 	bool find(char chr) {//probado
-		for (size_t i = 0; i < cola.size(); i++){
+		for (size_t i = 0; i < cola.size(); i++) {
 			if (cola[i]->getChar() == chr) {
 				cola[i]->increase_Freq();
 				return true;
@@ -64,4 +67,67 @@ public:
 		return false;
 	}
 
+	// Método para ordenar la cola usando MergeSort (de mayor a menor frecuencia)
+	void ordenarCola() {
+		if (cola.size() > 1) {
+			realizarMergeSort(cola, 0, cola.size() - 1);
+		}
+	}
+
+	// Función de MergeSort
+	void realizarMergeSort(vector<Nodo*>& listaNodos, int indiceInicio, int indiceFin) {
+		if (indiceInicio < indiceFin) {
+			int indiceMitad = (indiceInicio + indiceFin) / 2;
+			realizarMergeSort(listaNodos, indiceInicio, indiceMitad); // Ordenar first mitad
+			realizarMergeSort(listaNodos, indiceMitad + 1, indiceFin); // Ordenar second mitad
+			mezclarSublistas(listaNodos, indiceInicio, indiceMitad, indiceFin); // Mezclar both mitades
+		}
+	}
+
+	// Función para mezclar las dos mitades del vector de nodos
+	void mezclarSublistas(vector<Nodo*>& listaNodos, int indiceInicio, int indiceMitad, int indiceFin) {
+		int tamanoIzquierda = indiceMitad - indiceInicio + 1; // Tamaño de la sublista izquierda
+		int tamanoDerecha = indiceFin - indiceMitad; // Tamaño de la sublista derecha
+
+		// Subvectores temporales
+		vector<Nodo*> subListaIzquierda(tamanoIzquierda);
+		vector<Nodo*> subListaDerecha(tamanoDerecha);
+
+		// Copiar data subvectores temporales
+		for (int indiceIzquierda = 0; indiceIzquierda < tamanoIzquierda; indiceIzquierda++)
+			subListaIzquierda[indiceIzquierda] = listaNodos[indiceInicio + indiceIzquierda];
+		for (int indiceDerecha = 0; indiceDerecha < tamanoDerecha; indiceDerecha++)
+			subListaDerecha[indiceDerecha] = listaNodos[indiceMitad + 1 + indiceDerecha];
+
+		int indiceIzquierda = 0;
+		int indiceDerecha = 0;
+		int indiceMezcla = indiceInicio;
+
+		// Mezclar
+		while (indiceIzquierda < tamanoIzquierda && indiceDerecha < tamanoDerecha) {
+			if (subListaIzquierda[indiceIzquierda]->getFreq() >= subListaDerecha[indiceDerecha]->getFreq()) {
+				listaNodos[indiceMezcla] = subListaIzquierda[indiceIzquierda];
+				indiceIzquierda++;
+			}
+			else {
+				listaNodos[indiceMezcla] = subListaDerecha[indiceDerecha];
+				indiceDerecha++;
+			}
+			indiceMezcla++;
+		}
+
+		// Copiar subIzq
+		while (indiceIzquierda < tamanoIzquierda) {
+			listaNodos[indiceMezcla] = subListaIzquierda[indiceIzquierda];
+			indiceIzquierda++;
+			indiceMezcla++;
+		}
+
+		// Copiar subder
+		while (indiceDerecha < tamanoDerecha) {
+			listaNodos[indiceMezcla] = subListaDerecha[indiceDerecha];
+			indiceDerecha++;
+			indiceMezcla++;
+		}
+	}
 };
--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,39 @@
 #include "Huffman_Tree.h"
 using namespace std;
 
+void mostrarMenu() {
+	cout << "\n--- Menu ---\n";
+	cout << "1. Mostrar cola de prioridades\n";
+	cout << "2. Mostrar tabla de frecuencias\n";
+	cout << "3. Salir\n";
+	cout << "Seleccione una opción: ";
+}
+
 int main() {
-	Huffman_Tree x;
-	x.lectura_txt("ppp.txt");
+	Huffman_Tree huffmanTree;
+	int opcion = 0;
+	string filename = "ppp.txt";  // Nombre del archivo de texto a leer
+	huffmanTree.lectura_txt(filename);  // Generar tabla de frecuencias
+
+	do {
+		mostrarMenu();
+		cin >> opcion;
+		switch (opcion) {
+		case 1:
+			cout << "\n--- Cola de Prioridades (Ordenada) ---\n";
+			huffmanTree.ordenarCola();
+			huffmanTree.print();  // Mostrar cola de prioridades ordenada
+			break;
+		case 2:
+			cout << "\n--- Tabla de Frecuencias ---\n";
+			huffmanTree.print();  // Mostrar tabla de frecuencias
+			break;
+		case 3:
+			cout << "\n--- Saliendo del Programa... ---\n";
+			break;
+		default:
+			cout << "Opción no válida. Intente nuevamente.\n";
+		}
+	} while (opcion != 3);
+	return 0;
 }
